4112e1deaa856e2f6f4600a2143b73dd198e9af2
rottenpen <rottenpen@outlook.com>
2022-07-31 17:47:36 +0800

    feat: deno blog 版本

--- main.ts
+++ main.ts
@@ -1,4 +1,4 @@
-import blog from "https://deno.land/x/blog@0.3.3/blog.tsx";
+import blog from "https://deno.land/x/blog@0.4.2/blog.tsx";
 
 blog({
   title: "FE News 周刊",
@@ -9,12 +9,7 @@ blog({
     { title: "Email", url: "mailto:rottenpen@outlook.com" },
     { title: "GitHub", url: "https://github.com/rottenpen" },
   ],
-  background: "#fff",
+  favicon: "./goal.png",
   middlewares: [
-    // ga("UA-91675022-1"),
-    // redirects({
-    //   "iocp-links.html": "iocp_links",
-    //   "rant.html": "rant",
-    // }),
   ],
 });
